62171b3f19e32d4893a1ef2a33c110fad64bf633
MonBet <kudampas@gmail.com>
2021-03-23 15:12:05 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# inflasi
+# crypto-forecast
+Inflation predictor using MultilayerPerceptron\
+[GreedIsGood](http://inflasi.herokuapp.com/)
+
+# Deploy model
+pickled model from sklearn
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open("model/model_bintang.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Prediksi inflasi: {}%'.format(output))
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
